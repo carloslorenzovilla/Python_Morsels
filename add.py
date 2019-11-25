d185fb7fad00f8954dfa5a3e21a626f1e8f869a0
Carlos Villa <carloslorenzovilla@gmail.com>
2019-11-25 00:55:16 -0800

    Create add.py

---
+++ add.py
@@ -0,0 +1,19 @@
+# I'd like you to write a function that accepts two lists-of-lists 
+# of numbers and returns one list-of-lists with each of the 
+# corresponding numbers in the two given lists-of-lists added together.
+
+def add(*matrix):
+    """ Calculates elementwise sum of two or more matricies
+
+        *matrix: tuple of matricies
+
+        returns: list of lists matrix
+    """
+    element_sum = [
+        [
+            sum(m) 
+            for m in zip(*n)
+        ]  
+        for n in zip(*matrix)
+    ]
+    return element_sum
