c0461a7b4d2d217efd48f190797383a7679fb167
Carlos Villa <carloslorenzovilla@gmail.com>
2019-12-01 23:30:49 -0800

    Update

--- add.py
+++ add.py
@@ -2,18 +2,23 @@
 # of numbers and returns one list-of-lists with each of the 
 # corresponding numbers in the two given lists-of-lists added together.
 
-def add(*matrix):
-    """ Calculates elementwise sum of two or more 2D matricies
+def add(*matricies):
+    """ Calculates elementwise sum of two or more 2D matricies.
 
         *matrix: tuple of matricies
 
         returns: list of lists matrix
     """
+    mat_shape = {
+        tuple(len(x) for x in matrix) 
+        for matrix in matricies
+        }
+
+    if len(mat_shape) > 1:
+        raise ValueError('Given matricies are not the same size')
+
     element_sum = [
-        [
-            sum(m) 
-            for m in zip(*n)
-        ]  
-        for n in zip(*matrix)
+        [sum(m) for m in zip(*n)]  
+        for n in zip(*matricies)
     ]
     return element_sum
