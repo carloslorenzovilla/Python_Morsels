e4c0e7361dcf976724544f3dad42e5d326fa4f3d
Carlos Villa <carloslorenzovilla@gmail.com>
2019-12-03 00:22:27 -0800

    Initial

---
+++ count.py
@@ -0,0 +1,15 @@
+# I want you to write a function that accepts a string 
+# and returns a mapping (a dictionary or dictionary-like structure) 
+# that has words as the keys and the number of times each word was 
+# seen as the values.
+
+import re
+
+def count_words(string_):
+    regex = r'\w+'
+    string_ = re.findall(regex, string_)
+    dict_ = {}
+    for word in string_:
+        word = word.lower()
+        dict_[word] = 1 + dict_.get(word, 0)
+    return dict_
