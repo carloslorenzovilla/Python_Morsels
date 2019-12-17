085132484279a72e197b0f52be7252d9d78df21b
Carlos Villa <carloslorenzovilla@gmail.com>
2019-12-16 19:12:21 -0800

    update

--- circle.py
+++ circle.py
@@ -1,1 +1,26 @@
+from math import pi
+
+class Circle:
+    def __init__(self, rad=1):
+        self.radius = rad
+
+    def __str__(self):
+        return (f'Circle({self.radius})')
+
+    def __repr__(self):
+        return (f'Circle({self.radius})')
+    
+    @property
+    def area(self):
+        return pi*(self.radius**2)
+
+    @property
+    def diameter(self):
+        return self.radius*2
+
+    @diameter.setter
+    def diameter(self, d):
+        self.radius = d/2
+
+    
 
