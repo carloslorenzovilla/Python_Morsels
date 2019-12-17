909afa26fbd9c15b07697b3372d620445be30538
Carlos Villa <carloslorenzovilla@gmail.com>
2019-12-16 16:18:33 -0800

    update

--- uniques.py
+++ uniques.py
@@ -2,7 +2,7 @@
 # an iterable and returns a new iterable with 
 # all items from the original iterable except 
 # for duplicates.
-import collections
+from collections.abc import Hashable
 
 def uniques_only(list_):
     """ Returns iterator of unique items 
@@ -12,8 +12,17 @@ def uniques_only(list_):
 
         returns: generator (iterator)
     """
-    gen = set()
-    return (x for x in list_ if not (x in gen or gen.add(x)))
+    hash_seen = set()
+    not_hash_seen = []
+    for item in list_:
+        if isinstance(item, Hashable):
+            if item not in hash_seen:
+                yield item
+                hash_seen.add(item)
+        else:
+            if item not in not_hash_seen:
+                yield item
+                not_hash_seen.append(item)
 
 if __name__ == "__main__":
     x = uniques_only([1, 2, 1, 2, 3, 3, 1, 4])
