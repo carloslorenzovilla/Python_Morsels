a5f7d8754e684f179fe74f9b520ebe6701debf3a
Carlos Villa <carloslorenzovilla@gmail.com>
2020-01-20 16:59:12 -0800

    update

--- anagram.py
+++ anagram.py
@@ -1,1 +1,15 @@
+import unicodedata
 
+def remove_accent(string):
+    return unicodedata.normalize('NFKD', string)
+
+def is_anagram(word1, word2):
+
+    def get_letters(string):
+        string = remove_accent(string.lower())
+        return sorted(
+            x for 
+            x in string 
+            if x.isalpha())
+    
+    return get_letters(word1) == get_letters(word2)
